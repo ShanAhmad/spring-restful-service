5d1bcf3c3f3417ee287cc5cc9bb2033de661142f
Shan Ahmad <shanahmad177@gmail.com>
2023-01-24 13:32:22 +0500

    Rest Web Services Course Project

---
+++ src/main/java/com/shan/rest/webservice/restfulwebservice/RestfulWebServiceApplication.java
@@ -0,0 +1,14 @@
+package com.shan.rest.webservice.restfulwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.shan.rest.webservice.restfulwebservice.controller.HelloWorldController;
+
+@SpringBootApplication
+public class RestfulWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebServiceApplication.class, args);
+	}
+
+}
