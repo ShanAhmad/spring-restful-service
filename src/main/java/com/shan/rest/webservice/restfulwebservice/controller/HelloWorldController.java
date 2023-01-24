0d8dd91f7ad4bed31fa5677e4723a87b6e135691
Shan Ahmad <shanahmad177@gmail.com>
2023-01-24 14:20:25 +0500

    Rest Web Services Course Project Step : 06 Path Variables

--- src/main/java/com/shan/rest/webservice/restfulwebservice/controller/HelloWorldController.java
+++ src/main/java/com/shan/rest/webservice/restfulwebservice/controller/HelloWorldController.java
@@ -1,9 +1,6 @@
 package com.shan.rest.webservice.restfulwebservice.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloWorldController {
@@ -13,5 +10,10 @@ public class HelloWorldController {
     @GetMapping(path="/hello-world-bean")
     public HelloWorldBean helloWorldBean(){
         return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping(path="/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBean(@PathVariable String name){
+        return new HelloWorldBean(String.format("Hello, %s",name));
     }
 }
