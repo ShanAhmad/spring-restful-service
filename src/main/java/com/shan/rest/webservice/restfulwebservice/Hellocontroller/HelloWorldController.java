cd2151d02c2633a6cc02ad649455d13c6c098caf
Shan Ahmad <shanahmad177@gmail.com>
2023-01-26 22:13:19 +0500

    Rest Web Services Course Project Step : 18 Internationalization Added

--- src/main/java/com/shan/rest/webservice/restfulwebservice/Hellocontroller/HelloWorldController.java
+++ src/main/java/com/shan/rest/webservice/restfulwebservice/Hellocontroller/HelloWorldController.java
@@ -1,9 +1,15 @@
 package com.shan.rest.webservice.restfulwebservice.Hellocontroller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+    @Autowired
+    private MessageSource messageSource;
     @GetMapping(path = "/hello-world")
     public String helloworld(){return "Hello World";}
 
@@ -15,5 +21,9 @@ public class HelloWorldController {
     @GetMapping(path="/hello-world-bean/path-variable/{name}")
     public HelloWorldBean helloWorldBean(@PathVariable String name){
         return new HelloWorldBean(String.format("Hello, %s",name));
+    }
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalization(@RequestHeader(name = "Accept-Language", required = false) Locale locale){
+        return messageSource.getMessage("good.morning.message",null,locale);
     }
 }
