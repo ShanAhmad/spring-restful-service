089170fe02b62e425900f58873806b58098233ca
Shan Ahmad <shanahmad177@gmail.com>
2023-04-26 11:53:24 +0500

    test changes

--- src/main/java/com/shan/rest/webservice/restfulwebservice/Hellocontroller/HelloWorldController.java
+++ src/main/java/com/shan/rest/webservice/restfulwebservice/Hellocontroller/HelloWorldController.java
@@ -15,12 +15,12 @@ public class HelloWorldController {
 
     @GetMapping(path="/hello-world-bean")
     public HelloWorldBean helloWorldBean(){
-        return new HelloWorldBean("Hellos World");
+        return new HelloWorldBean("Helloss World");
     }
 
     @GetMapping(path="/hello-world-bean/path-variable/{name}")
     public HelloWorldBean helloWorldBean(@PathVariable String name){
-        return new HelloWorldBean(String.format("Hellos, %s",name));
+        return new HelloWorldBean(String.format("Helloss, %s",name));
     }
     @GetMapping(path = "/hello-world-internationalized")
     public String helloWorldInternationalization(@RequestHeader(name = "Accept-Language", required = false) Locale locale){
